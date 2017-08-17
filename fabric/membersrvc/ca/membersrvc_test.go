bb7dcbd59f2dbba2883f7af41d5edac2303ea85c
Agzs <gzs_1994@163.com>
2017-08-17 22:46:31 +0800

    add comments on membersrvc_test.go, it test package membersrvc

--- fabric/membersrvc/ca/membersrvc_test.go
+++ fabric/membersrvc/ca/membersrvc_test.go
@@ -39,73 +39,81 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	setupTestConfig()
-	curve := primitives.GetDefaultCurve()
+	setupTestConfig()                     // 配置设置
+	curve := primitives.GetDefaultCurve() // 获取用于加密的椭圆曲线
 	fmt.Printf("Default Curve %v \n", curve)
 	// Init PKI
-	initPKI()
-	go startPKI()
-	defer cleanup()
-	time.Sleep(time.Second * 10)
+	initPKI()                    // 初始化PKI
+	go startPKI()                // 启动PKI
+	defer cleanup()              // 最后执行，收尾工作
+	time.Sleep(time.Second * 10) // 线程Sleep的10秒
 	fmt.Println("Running tests....")
-	ret := m.Run()
+	ret := m.Run() // 运行测试程序
 	fmt.Println("End running tests....")
-	cleanupFiles()
-	os.Exit(ret)
+	cleanupFiles() // 文件收尾处理
+	os.Exit(ret)   // 退出测试程序
 
 }
 
+// 设置测试配置
 func setupTestConfig() {
-	primitives.SetSecurityLevel("SHA3", 256)
-	viper.AutomaticEnv()
-	viper.SetConfigName("ca_test") // name of config file (without extension)
-	viper.AddConfigPath("./")      // path to look for the config file in
-	viper.AddConfigPath("./..")    // path to look for the config file in
-	err := viper.ReadInConfig()    // Find and read the config file
-	if err != nil {                // Handle errors reading the config file
+	primitives.SetSecurityLevel("SHA3", 256) // 设置安全配置文件算法采用SHA3，hash长度为256
+	viper.AutomaticEnv()                     // 检查设置在config，default，flags中的所有键的ENV变量
+	viper.SetConfigName("ca_test")           // name of config file (without extension)
+	viper.AddConfigPath("./")                // path to look for the config file in
+	viper.AddConfigPath("./..")              // path to look for the config file in
+	err := viper.ReadInConfig()              // Find and read the config file
+	if err != nil {                          // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 }
 
+// 初始化PKI
 func initPKI() {
 	CacheConfiguration() // Cache configuration
-	aca = NewACA()
+	aca = NewACA()       // 创建实例
 	eca = NewECA(aca)
 	tca = NewTCA(eca)
 }
 
+// 启动PKI
 func startPKI() {
 	var opts []grpc.ServerOption
 	fmt.Printf("open socket...\n")
+	// GetString将与参数相关联的值作为字符串laddr返回； Listen监听本地网络地址上laddr的通报
 	sockp, err := net.Listen("tcp", viper.GetString("server.port"))
 	if err != nil {
 		panic("Cannot open port: " + err.Error())
 	}
 	fmt.Printf("open socket...done\n")
 
-	server = grpc.NewServer(opts...)
-	aca.Start(server)
+	server = grpc.NewServer(opts...) // 新建服务
+	aca.Start(server)                // 启动服务
 	eca.Start(server)
 	tca.Start(server)
 	fmt.Printf("start serving...\n")
-	server.Serve(sockp)
+	server.Serve(sockp) // Serve接受监听器sockp上的传入连接，为每个服务器创建一个新的服务传输和服务程序(goroutine)。
+	// 服务程序读取gRPC请求，然后调用注册的处理程序来处理它们。 sockp.Accept失败时返回服务。 当此方法返回时，sock将被关闭。
 }
 
+// 收尾工作
 func cleanup() {
 	fmt.Println("Cleanup...")
 	stopPKI()
 	fmt.Println("Cleanup...done!")
 }
 
+// 清理文件
 func cleanupFiles() {
 	//cleanup files
-	path := viper.GetString("server.cadir")
-	err := os.RemoveAll("./" + path)
+	path := viper.GetString("server.cadir") // 获取路径
+	err := os.RemoveAll("./" + path)        // 删除文件爱呢
 	if err != nil {
 		fmt.Printf("Failed removing [%s] [%s]\n", path, err)
 	}
 }
 
+// 停止PKI服务
 func stopPKI() {
 	aca.Stop()
 	eca.Stop()
